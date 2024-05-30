4ae65f4f0127d00a384d5a7dba114b7e9e2f1841
heryjs <hery.julianto@nawadata.com>
2024-05-30 19:46:17 +0700

    Category

---
+++ OnlineStore/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Data;
+using OnlineStore.Models;
+
+namespace OnlineStore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = _db.Category.ToList();
+            return View(categories);
+        }
+    }
+}
---
+++ OnlineStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStore.Models;
+
+namespace OnlineStore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Category { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId=1, Name="Action", DisplayOrder=1},
+                new Category { CategoryId=2, Name="SciFi", DisplayOrder=2},
+                new Category { CategoryId=3, Name="Romance", DisplayOrder=3},
+                new Category { CategoryId=4, Name="History", DisplayOrder=4},
+                new Category { CategoryId=5, Name="War", DisplayOrder=5}
+                );
+        }
+    }
+}
