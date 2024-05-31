19b5998b234d882b820362ac030616a74a5ed4ae
heryjs <hery.julianto@nawadata.com>
2024-05-31 10:08:26 +0700

    create view

--- OnlineStore/Controllers/CategoryController.cs
+++ OnlineStore/Controllers/CategoryController.cs
@@ -4,17 +4,22 @@ using OnlineStore.Models;
 
 namespace OnlineStore.Controllers
 {
-    public class CategoryController : Controller
-    {
-        private ApplicationDbContext _db;
-        public CategoryController(ApplicationDbContext db)
-        {
-            _db = db;
-        }
-        public IActionResult Index()
-        {
-            List<Category> categories = _db.Category.ToList();
-            return View(categories);
-        }
-    }
+	public class CategoryController : Controller
+	{
+		private ApplicationDbContext _db;
+		public CategoryController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+		public IActionResult Index()
+		{
+			List<Category> categories = _db.Category.ToList();
+			return View(categories);
+		}
+
+		public IActionResult Create()
+		{
+			return View();
+		}
+	}
 }
